bdf5d5ae9cd1a7f5fb361d6acd8a445b0c33f14e
benjackson3811 <benjackson3811@googlemail.com>
2023-03-13 15:26:49 +0000

    front screen and instructions functions added

---
+++ quiz_questions.py
@@ -0,0 +1,26 @@
+questions = {
+    "What other name is Cape Town known as?: ": "A",
+    "Which provenice is the city located?: ": "A",
+    "What year was the city founded?: ": "C",
+    "What Language is is majorly used in the city?: ": "D",
+    "Which of these was instrutmental in the creation of the city?: ": "D",
+    "When did Cape Town host the football world cup?: ": "B",
+    "What is the name of the ocean that surrounds Cape Town?: ": "A",
+    "What Alcholic beverage is the Cape famous for producing? ": "D",
+    "In which part of South Africa in Cape Town located? ": "A",
+    "What is mined at Kimberley in South Africa? ": "D"
+}
+
+options = [
+    ["A. Mother City", "B. New City", "C. Kaap Stad", "D. The City!"],
+    ["A. Western Cape", "B. Cape Fear", "C. Cape Beer ", "D. Africa"],
+    ["A. 1625", "B. 1642", "C. 1652", "D. 1632"],
+    ["A. Bandu", "B. Xhorsa", "C. Afikaans", "D. English"],
+    ["A. HSBC", "B. Coke", "C. Gap", "D. Dutch East India Company"],
+    ["A, 2006", "B. 2010", "C. 2014", "D. 2018"],
+    ["A. Atlantic", "B. Indian", "C. Artic", "D. Pacific"],
+    ["A. Whiskey", "B. Vodka", "C. Sake", "D. Wine"],
+    ["A. South", "B. East", "C. West", "D. North"],
+    ["A. Wine", "B. Silver", "C. Gold", "D. Diamonds"],
+
+]
--- run.py
+++ run.py
@@ -2,10 +2,68 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
-import questions
+import sys
+import os
+import time
+from quiz_questions import questions
+from quiz_questions import options
+
+
+def display_menu():
+    """
+    function to show the user the menu options avaliable to them.
+
+    """
+    print("-----------------------------")
+    print()
+    print()
+    print("Welcome to the Cape Town Quiz")
+    print()
+    print()
+    print("-----------------------------")
+    clear_screen()
+    time.sleep(2)
+    print()
+    print()
+
+    selection = input("""
+    -----------------------------
+    1: Start Quiz
+    2: Game Instructions
+    3: High Score
+    -----------------------------
+    Please enter a choice
+    1, 2 or 3
+    -----------------------------
+    """)
+    if selection == "1":
+        clear_screen()
+        new_game()
+    elif selection == "2":
+        clear_screen()
+        game_instructions()
+        display_menu()
+    elif selection == "3":
+        sys.exit
+    else:
+        clear_screen()
+        print("Invalid Selection - please select one of the the three options")
+        return display_menu()
+
+
+# -------------------
+def clear_screen():
+    """
+    function to clear screen
+    """
+    os.system('clear')
+
 
 # -------------------
 def new_game():
+    """
+    function to load up the game, define the questions to be asked
+    """
     guesses = []
     correct_guesses = 0
     question_num = 1
@@ -26,12 +84,29 @@ def new_game():
 
 
 # -------------------
+def game_instructions():
+    """
+    game instructions for the user
+    """
+    print("There are three quiz topics you can asnwer questions on")
+    print(" 1: Cape Town History")
+    print(" 2: The Sea. ")
+    print(" South African Land Answers")
+    print(" For each topic, there are 5 multiple choice questions.")
+    print(" You have four options, A, B, C or D")
+    print(" If your answer is correct, the system will tell you!")
+    print()
+    print(" Press any button to go back to the main screen")
+    clear_screen()
+
+
+# -------------------
 def check_answer(answer, guess):
     if answer == guess:
         print("⭐ Correct! ⭐!")
         return 1
     else:
-        print("Wrong! The correct answer is " +str(answer))
+        print("Wrong! The correct answer is " + str(answer))
         return 0
 
 
@@ -57,7 +132,7 @@ def display_score(correct_guesses, guesses):
 # -------------------
 def play_again():
     response = input("Do you want to play again? (yes or no): ")
-    lower_response = response.lower()  # Change to lower
+    lower_response = response.lower()
 
     if lower_response == "yes":  # Change "yes" to lower as well
         return True
@@ -66,23 +141,15 @@ def play_again():
 # -------------------
 
 
-questions = {
-    "What other name is Cape Town known as?: ": "A",
-    "Which provenice is the city located?: ": "A",
-    "What year was the city founded?: ": "C",
-    "What Language is is majorly used in the city?: ": "D",
-    "Which of these was instrutmental in the creation of the city?: ": "D"
-}
+def front_screen():
+    """shows the display menu function options for the user"""
 
-options = [["A. Mother City", "B. New City", "C. Kaap Stad", "D. The City!"],
-    ["A. Western Cape", "B. Cape Fear", "C. Cape Beer ", "D. Africa"],
-    ["A. 1625", "B. 1642", "C. 1652", "D. 1632"],
-    ["A. Bandu", "B. Xhorsa", "C. Afikaans", "D. English"],
-    ["A. HSBC", "B. Coke", "C. Gap", "D. Dutch East India Company"]]
+    display_menu()
 
-new_game()
+    while play_again():
+        new_game()
 
-while play_again():
-    new_game()
+    print("Thanks for playing!!!")
 
-print("Byyeee!!!")
+
+front_screen()
