d211000ce8e1e559cadc00d7bada7eea2e34ade9
benjackson3811 <benjackson3811@googlemail.com>
2023-03-13 09:21:50 +0000

    adding new_game function details

--- run.py
+++ run.py
@@ -5,12 +5,31 @@
 
 # -------------------
 def new_game():
-    pass
+    guesses = []
+    correct_guesses = 0
+    question_num = 1
+
+    for key in questions:
+        print("-------------------")
+        print(key)
+        for i in options[question_num-1]:
+            print(i)
+        guess = input("Enter (A, B, C, or D): ")
+        guess = guess.upper()
+        guesses.append(guess)
+
+        check_answer(questions.get(key,guess))
+        question_num += 1
 
 
 # -------------------
-def check_answer():
-    pass
+def check_answer(answer, guess):
+    if answer == guess:
+        print(Correct!)
+        return 1
+    else:
+        print(Wrong!)
+        return 0
 
 
 # -------------------
@@ -33,10 +52,10 @@ questions = {
     "Which of these was instrutmental in the creation of the city?: ": "D"
 }
 
-options = [
-    ["A. Mother City"], ["B. New City"], ["C. Kaap Stad"], ["D. The City!"],
-    ["A. Western Cape"], ["B. Cape Fear"], ["C. Cape Beer "], ["D. Africa"],
-    ["A. 1625"], ["B. 1642"], ["C. 1652"], ["D. 1632"],
-    ["A. Bandu"], ["B. Xhorsa"], ["C. Afikaans"], ["D. English"],
-    ["A. HSBC"], ["B. Coke"], ["C. Gap"], ["D. Dutch East India Company"]]
+options = [["A. Mother City", "B. New City", "C. Kaap Stad", "D. The City!"],
+    ["A. Western Cape", "B. Cape Fear", "C. Cape Beer ", "D. Africa"],
+    ["A. 1625", "B. 1642", "C. 1652", "D. 1632"],
+    ["A. Bandu", "B. Xhorsa", "C. Afikaans", "D. English"],
+    ["A. HSBC", "B. Coke", "C. Gap", "D. Dutch East India Company"]]
 
+new_game()
