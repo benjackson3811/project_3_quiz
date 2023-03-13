2e88dac17d5320b1d9d942c36332f1a07839457a
benjackson3811 <benjackson3811@googlemail.com>
2023-03-13 09:05:55 +0000

    adding inital questions and options

--- run.py
+++ run.py
@@ -1,3 +1,42 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+# -------------------
+def new_game():
+    pass
+
+
+# -------------------
+def check_answer():
+    pass
+
+
+# -------------------
+def display_score():
+    pass
+
+
+# -------------------
+def play_again():
+    pass
+
+# -------------------
+
+
+questions = {
+    "What other name is Cape Town known as?: ": "A",
+    "Which provenice is the city located?: ": "A",
+    "What year was the city founded?: ": "C",
+    "What Language is is majorly used in the city?: ": "D",
+    "Which of these was instrutmental in the creation of the city?: ": "D"
+}
+
+options = [
+    ["A. Mother City"], ["B. New City"], ["C. Kaap Stad"], ["D. The City!"],
+    ["A. Western Cape"], ["B. Cape Fear"], ["C. Cape Beer "], ["D. Africa"],
+    ["A. 1625"], ["B. 1642"], ["C. 1652"], ["D. 1632"],
+    ["A. Bandu"], ["B. Xhorsa"], ["C. Afikaans"], ["D. English"],
+    ["A. HSBC"], ["B. Coke"], ["C. Gap"], ["D. Dutch East India Company"]]
+
